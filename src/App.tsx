74958b2af72ca21fd6330f1711220cc84fde9d11
Debbl <me@aiwan.run>
2023-06-11 22:35:47 +0800

    feat: working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,76 @@
+import type { ElementRef, MouseEvent } from "react";
+import { useRef, useState } from "react";
+import frame0Path from "./assets/images/frame0.jpg";
+import frame1Path from "./assets/images/frame1.jpg";
+import frame2Path from "./assets/images/frame2.jpg";
+import frame3Path from "./assets/images/frame3.jpg";
+import PopSounds from "./components/PopSounds";
+
+type PopSoundsRef = ElementRef<typeof PopSounds>;
+const framesPath = [frame0Path, frame1Path, frame2Path, frame3Path];
+
 function App() {
-  return <div className="text-red-200">hello</div>;
+  const popSoundsRef = useRef<PopSoundsRef>(null);
+  const [framesBoard, setFramesBoard] = useState([...framesPath]);
+
+  const handleMousedown = (
+    e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>,
+    indexes: number[]
+  ) => {
+    e.preventDefault();
+    popSoundsRef.current?.playRandomSound();
+
+    const setNestedFrame = (
+      nestedArray: any[],
+      indexList: number[],
+      depth = 0
+    ) => {
+      if (depth === indexList.length - 1) {
+        nestedArray[indexList[depth]] = [...framesPath];
+      } else {
+        if (!Array.isArray(nestedArray[indexList[depth]])) {
+          nestedArray[indexList[depth]] = [];
+        }
+        setNestedFrame(nestedArray[indexList[depth]], indexList, depth + 1);
+      }
+    };
+
+    const next = [...framesBoard];
+    setNestedFrame(next, indexes);
+    setFramesBoard(next);
+  };
+
+  const renderFrames = (frames: any[], parentIndex?: string) => {
+    return frames.flatMap((frames, i) => {
+      const key = parentIndex ? `${parentIndex}-${i}` : `${i}`;
+      if (Array.isArray(frames)) {
+        return (
+          <div className="flex h-1/2 w-1/2 flex-wrap" key={key}>
+            {renderFrames(frames, key)}
+          </div>
+        );
+      } else {
+        return (
+          <div
+            className="h-1/2 w-1/2 select-none"
+            onMouseDown={(e) => handleMousedown(e, key.split("-").map(Number))}
+            key={key}
+          >
+            <img src={frames} alt="" className="select-none" />
+          </div>
+        );
+      }
+    });
+  };
+
+  return (
+    <div className="w-screen bg-[#A7C9B9]/100">
+      <div className="mx-auto flex h-screen w-[100vh] cursor-pointer flex-wrap justify-center">
+        {renderFrames(framesBoard)}
+      </div>
+      <PopSounds ref={popSoundsRef} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PopSounds.tsx
@@ -0,0 +1,41 @@
+import type { ForwardRefRenderFunction } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import bubbles0Path from "../assets/sounds/bubbles0.mp3";
+import bubbles1Path from "../assets/sounds/bubbles1.mp3";
+import bubbles2Path from "../assets/sounds/bubbles2.mp3";
+import bubbles3Path from "../assets/sounds/bubbles3.mp3";
+
+const PopSounds: ForwardRefRenderFunction<{
+  playRandomSound: () => void;
+}> = (_, ref) => {
+  const bubbles = [bubbles0Path, bubbles1Path, bubbles2Path, bubbles3Path];
+  const audioElementsRefs = useRef<HTMLAudioElement[]>([]);
+
+  const playRandomSound = () => {
+    const i = Math.floor(Math.random() * 4);
+    audioElementsRefs.current[i].play();
+  };
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      playRandomSound,
+    }),
+    []
+  );
+  return (
+    <div className="hidden">
+      {bubbles.map((b, i) => (
+        <audio
+          key={i}
+          src={b}
+          ref={(e) => (audioElementsRefs.current[i] = e!)}
+        ></audio>
+      ))}
+    </div>
+  );
+};
+
+PopSounds.displayName = "PopSounds";
+
+export default forwardRef(PopSounds);
